3ba4455cf66eb7f9c0ee605ccef9addde373bf87
sonihidayatulloh797 <sonihidayatulloh797@gmail.com>
2025-09-27 17:09:28 +0700

    add model tamplate surat

--- app/Models/TemplateSurat.php
+++ app/Models/TemplateSurat.php
@@ -7,9 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class TemplateSurat extends Model
 {
     protected $table = 'template_surat';
+
     protected $fillable = [
         'nama_template',
         'kategori',
         'file_template'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function template()
+    {
+        return $this->belongsTo(TemplateSurat::class, 'template_id');
+    }
 }
